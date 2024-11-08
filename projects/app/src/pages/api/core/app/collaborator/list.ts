b831c37afb179298622afdcacd6307232a2ac6b3
shilin66 <15151873958@163.com>
2024-11-08 10:46:55 +0800

    fix: 应用或者知识库查询协作者的权限问题

--- projects/app/src/pages/api/core/app/collaborator/list.ts
+++ projects/app/src/pages/api/core/app/collaborator/list.ts
@@ -2,13 +2,13 @@ import { NextApiRequest } from 'next';
 import type { ApiResponseType } from '@fastgpt/service/type/next';
 import { NextAPI } from '@/service/middleware/entry';
 import { listAppCollaborator } from '@fastgpt/service/support/permission/app/controller';
-import { ManagePermissionVal } from '@fastgpt/global/support/permission/constant';
+import { ReadPermissionVal } from '@fastgpt/global/support/permission/constant';
 import { authApp } from '@fastgpt/service/support/permission/app/auth';
 
 async function handler(req: NextApiRequest, res: ApiResponseType<any>) {
   const appId = req.query.appId as string;
 
-  await authApp({ req, authToken: true, appId, per: ManagePermissionVal });
+  await authApp({ req, authToken: true, appId, per: ReadPermissionVal });
 
   return listAppCollaborator(appId);
 }
