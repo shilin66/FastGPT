635c28908698768964f5ee8e7435e0063316e173
shilin66 <15151873958@163.com>
2024-11-08 10:46:55 +0800

    fix: 应用或者知识库查询协作者的权限问题

--- projects/app/src/pages/api/core/dataset/collaborator/list.ts
+++ projects/app/src/pages/api/core/dataset/collaborator/list.ts
@@ -1,14 +1,17 @@
 import { NextApiRequest } from 'next';
 import type { ApiResponseType } from '@fastgpt/service/type/next';
 import { NextAPI } from '@/service/middleware/entry';
-import { ManagePermissionVal } from '@fastgpt/global/support/permission/constant';
+import {
+  ManagePermissionVal,
+  ReadPermissionVal
+} from '@fastgpt/global/support/permission/constant';
 import { listDatasetCollaborator } from '@fastgpt/service/support/permission/dataset/controller';
 import { authDataset } from '@fastgpt/service/support/permission/dataset/auth';
 
 async function handler(req: NextApiRequest, res: ApiResponseType<any>) {
   const datasetId = req.query.datasetId as string;
 
-  await authDataset({ req, authToken: true, datasetId, per: ManagePermissionVal });
+  await authDataset({ req, authToken: true, datasetId, per: ReadPermissionVal });
 
   return listDatasetCollaborator(datasetId);
 }
