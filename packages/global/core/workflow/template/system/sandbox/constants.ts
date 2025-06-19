c996c0155f230bc65abe07f2889f50febb3af86a
shilin66 <15151873958@163.com>
2025-06-19 10:06:21 +0800

    feat: 修改sandbox的python代码模板

--- packages/global/core/workflow/template/system/sandbox/constants.ts
+++ packages/global/core/workflow/template/system/sandbox/constants.ts
@@ -6,8 +6,14 @@ export const JS_TEMPLATE = `function main({data1, data2}){
   }
 }`;
 
-export const PY_TEMPLATE = `def main(data1: str, data2: str) -> dict:
-    
+// export const PY_TEMPLATE = `def main(data1: str, data2: str) -> dict:
+//
+//     return {
+//         "result": data1,
+//         "data2": data2
+//     }`;
+
+export const PY_TEMPLATE = `def main(data1, data2):
     return {
         "result": data1,
         "data2": data2
